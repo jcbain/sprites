0b695a32ed5938989c73542fbda3335778c06812
James Bain <james.bain@lighthouselabs.com>
2021-09-19 19:51:05 -0600

    update state for direction and movement

--- src/components/Sprite.js
+++ src/components/Sprite.js
@@ -17,10 +17,10 @@ const SpriteImg = styled.img`
 
 const Sprite = (props) => {
   const [ leftPosition, setLeftPosition ] = useState(0);
-  const [ mode ] = useState('walk');
-  const [ animal ] = [{name: 'dog_2', pixels: 48}]
+  const [ mode ] = useState('attack');
+  const [ animal ] = [{name: 'cat_2', pixels: 48}]
   const [ direction, setDirection ] = useState(1)
-
+  const [ moveFrame, setMoveFrame ] = useState({direction: 1, leftPosition: 0})
   
   useEffect(() => {
     const modeStates = {
@@ -32,24 +32,38 @@ const Sprite = (props) => {
     }
     const movePlayer = e => {
       console.log(e.keyCode)
-
-      if(e.keyCode === 39){
-        setDirection(1);
-        setLeftPosition( prev => {
-          if(prev >= (modeStates[mode] * animal.pixels) - animal.pixels ) {
-            return 0
+      setMoveFrame(prev => {
+        const updateFrame = {}
+        
+        if(e.keyCode === 39){
+          updateFrame.direction = 1;
+          if(prev.leftPosition >= (modeStates[mode] * animal.pixels) - animal.pixels ) {
+            updateFrame.leftPosition = 0;
+          } else {
+            updateFrame.leftPosition = prev.leftPosition + animal.pixels;
           }
-          return prev + (animal.pixels * direction);
-        })
-      }
+        }
+
+        if(e.keyCode === 37){
+          updateFrame.direction = -1
+          if(prev.leftPosition <= 0) {
+            updateFrame.leftPosition = (modeStates[mode] * animal.pixels) - animal.pixels;
+          } else {
+            updateFrame.leftPosition = prev.leftPosition - animal.pixels;
+          }
+        }
+
+        return updateFrame;
+      })
     }
+
     document.addEventListener('keydown', movePlayer)
 
     return () => {
       document.removeEventListener('keydown', movePlayer)
     }
 
-  }, [mode, animal, direction])
+  }, [mode, animal, moveFrame])
 
 
 
@@ -60,8 +74,8 @@ const Sprite = (props) => {
     >
       <SpriteImg className="sprite" 
         src={`images/${animal.name}/${mode}.png`} 
-        leftpos={leftPosition} 
-        dir={direction}
+        leftpos={moveFrame.leftPosition} 
+        dir={moveFrame.direction}
       />
     </Container>
   )
